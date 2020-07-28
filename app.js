69234610c78670d33497cbcae5ab357f682f5744
yuriyberezskyy <bereza95@gmail.com>
2020-07-28 16:11:43 -0400

    checking data from weather  API

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const request = require("request");
 const url =
   "http://api.weatherstack.com/current?access_key=378ded7c846cfd0a53dfadaed48d1df5&query=37.8267,-122.4233";
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+  console.log(response.body.current);
+  console.log(
+    "It is currently " +
+      response.body.current.temperature +
+      ". It feels like " +
+      response.body.current.temperature
+  );
 });
