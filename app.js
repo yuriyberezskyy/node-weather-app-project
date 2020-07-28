187a07903b6ee636aeb655e87637a306d0feae8f
yuriyberezskyy <bereza95@gmail.com>
2020-07-28 16:18:50 -0400

    added temerature in F '&units=f'

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const request = require("request");
 //http://api.weatherstack.com/current?access_key=378ded7c846cfd0a53dfadaed48d1df5&query=37.8267,-122.4233
 
 const url =
-  "http://api.weatherstack.com/current?access_key=378ded7c846cfd0a53dfadaed48d1df5&query=37.8267,-122.4233";
+  "http://api.weatherstack.com/current?access_key=378ded7c846cfd0a53dfadaed48d1df5&query=37.8267,-122.4233&units=f";
 
 request({ url: url, json: true }, (error, response) => {
-  console.log(response.body.current);
+  // console.log(response.body.current);
   console.log(
     "It is currently " +
       response.body.current.temperature +
