91008600be269b31a8d9c40532cad038fbe208b4
yuriyberezskyy <bereza95@gmail.com>
2020-08-04 13:37:01 -0400

    changes in app.js with forecast file

--- app.js
+++ app.js
@@ -8,8 +8,15 @@ console.log("The weather for today: ");
 geocode("Brooklyn", (error, data) => {
   console.log("Error", error);
   console.log("Data", data);
-  forecast(data.latitude, data.longitude, (error, data) => {
-    console.log("Error:", error);
-    console.log("Data:", data);
-  });
+  if (!data) {
+    forecast(undefined, undefined, (error, data) => {
+      console.log("Error:", error);
+      console.log("Data:", data);
+    });
+  } else {
+    forecast(data.latitude, data.longitude, (error, data) => {
+      console.log("Error:", error);
+      console.log("Data:", data);
+    });
+  }
 });
