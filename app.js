5ae737a8c672d05eda8a0e9d33cae6a3e5d8f164
yuriyberezskyy <bereza95@gmail.com>
2020-08-04 14:06:54 -0400

    added location checker

--- app.js
+++ app.js
@@ -3,20 +3,24 @@ const request = require("request");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-console.log("The weather for today: ");
+if (!process.argv[2]) {
+  console.log("Put location");
+} else {
+  console.log("The weather for today: ");
 
-geocode(process.argv[2], (error, data) => {
-  if (error) {
-    return console.log(error);
-  }
-
-  forecast(data.latitude, data.longitude, (error, forecastData) => {
+  geocode(process.argv[2], (error, data) => {
     if (error) {
-      console.log(error);
+      return console.log(error);
     }
 
-    console.log("Location: " + data.location);
-    console.log("Temperature: " + forecastData.temperature);
-    console.log("Feels like: " + forecastData.feelslike);
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        console.log(error);
+      }
+
+      console.log("Location: " + data.location);
+      console.log("Temperature: " + forecastData.temperature);
+      console.log("Feels like: " + forecastData.feelslike);
+    });
   });
-});
+}
