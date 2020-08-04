d9f562e70ae9ae3cca576de51ff879c50f5ce1f0
yuriyberezskyy <bereza95@gmail.com>
2020-08-03 23:49:45 -0400

    the weather for today

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const request = require("request");
 // "http://api.weatherstack.com/current?access_key=378ded7c846cfd0a53dfadaed48d1df5&query=37.8267,-122.4233&units=f"
+console.log("The weather for today: ");
 
 const url =
   "http://api.weatherstack.com/current?access_key=378ded7c846cfd0a53dfadaed48d1df5&query=37.8267,-122.4233&units=f";
