4bfcfd51180cae869bbd45bb539ada107f5fbe5b
yuriyberezskyy <bereza95@gmail.com>
2020-08-04 13:24:44 -0400

    added forecast

--- app.js
+++ app.js
@@ -5,27 +5,11 @@ const forecast = require("./utils/forecast");
 
 console.log("The weather for today: ");
 
-// const url =
-//   "http://api.weatherstack.com/current?access_key=378ded7c846cfd0a53dfadaed48d1df5&query=37.8267,-122.4233&units=f";
-
-// request({ url: url, json: true }, (error, response) => {
-//   if (error) {
-//     console.log("Unable to connect to weather service!");
-//   } else if (response.body.error) {
-//     console.log("Unable to find location");
-//   } else {
-//     console.log(
-//       "It is currently " +
-//         response.body.current.temperature +
-//         ". It feels like " +
-//         response.body.current.temperature
-//     );
-//   }
-// });
-
-geocode("Boston", (error, data) => {
+geocode("Brooklyn", (error, data) => {
   console.log("Error", error);
   console.log("Data", data);
+  forecast(data.latitude, data.longitude, (error, data) => {
+    console.log("Error:", error);
+    console.log("Data:", data);
+  });
 });
-
-forecast();
--- utils/forecast.js
+++ utils/forecast.js
@@ -3,12 +3,23 @@ const request = require("request");
 const forecast = (latitude, longitude, callback) => {
   const url =
     "http://api.weatherstack.com/current?access_key=378ded7c846cfd0a53dfadaed48d1df5&query=" +
-    latitude +
-    "," +
     longitude +
+    "," +
+    latitude +
     "&units=f";
 
-  request({ url: url, json: true }, (error, response));
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("No connection", undefined);
+    } else if (response.body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(undefined, {
+        feelslike: response.body.current.feelslike,
+        temperature: response.body.current.temperature,
+      });
+    }
+  });
 };
 
 module.exports = forecast;
